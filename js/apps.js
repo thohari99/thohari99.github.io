9ab62bca9bd6ce21b7b66d759909b71df51ffbe9
thohari99 <zkillua71@yahoo.com>
2015-09-30 13:42:16 +0800

    @wifi.id

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,9 @@
 $(function(){
 	for (i=0; i<data.length; i++){
-		$(".list-iklan").prepend("<div class='col-md-4 col-sm-6 col-xs-12'><a target='_blank' href='"+data[i].link+"'><div class='box "+data[i].warna+"'><h4 class='text text-center'>"+data[i].judul+"</h4></div><p>"+data[i].deskripsi+"</p></a></div>");
+		if (data[i].deskripsi == null){
+			data[i].deskripsi = "&nbsp;";
+		}
+		$(".list-iklan").prepend("<div class='col-md-3 col-sm-6 col-xs-12'><a target='_blank' href='"+data[i].link+"'><div class='box "+data[i].warna+"'><h4 class='text text-center'>"+data[i].judul+"</h4><p>"+data[i].deskripsi+"</p></div></a></div>");
 	}
 })
 
